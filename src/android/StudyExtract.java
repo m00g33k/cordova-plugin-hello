013ea1aa71d50c443d121616affb127e3ee703b5
Mike Shreek <m00g33k@gmail.com>
2016-11-25 06:53:27 +0800

    added extractFromFile command

--- src/android/StudyExtract.java
+++ src/android/StudyExtract.java
@@ -4,16 +4,49 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.security.Permission;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.io.BufferedReader;
+import java.io.FileReader;
 public class StudyExtract extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
+        if (action.equals("extractFromFile")) {
 
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
+
+
+            File file = new File(data.getString(0),data.getString(1));
+
+            //Read text from file
+            StringBuilder text = new StringBuilder();
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    text.append(line);
+                    text.append('\n');
+                }
+                br.close();
+                
+                        callbackContext.success("file success reading");
+            }
+            catch (IOException e) {
+                //You'll need to add proper error handling here
+                        callbackContext.error("file error reading");
+            }
+
 
             return true;
 
--- www/studyextract.js
+++ www/studyextract.js
@@ -3,5 +3,8 @@
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+    },
+    extractFromFile: function (folderPath, fileName, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "StudyExtract", "extractFromFile", [folderPath, fileName]);
     }
 };
